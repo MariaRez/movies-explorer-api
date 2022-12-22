ef3c3caeb249ca19145356dac81db627fd4d5a19
MariaRez <maria.v.reznik@yandex.ru>
2022-12-22 17:58:14 +0400

    refactor: ratelimiter moved to separate file

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,6 +9,7 @@ const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const handlerErrors = require('./middlewares/handlerErrors');
 const NotFoundError = require('./errors/NotFoundError');
+const { limiter } = require('./middlewares/rateLimiter');
 
 const options = { // для cors настройки
   origin: [
@@ -29,13 +29,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(helmet());
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
 
 app.use(limiter);
 
